4c1f037db6d091f4fb512848b66775ab548c3fd7
Jefferson Duarte <jeffersonsduarte@live.com>
2022-08-24 11:01:32 +0100

    Adicionado função editar e deletar

--- produto/urls.py
+++ produto/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProdutoCreate.as_view(), name='cadastro_produto'),
-    path('lista/', views.ProdutoLista.as_view(), name='lista_produtos')
+    path('lista/', views.ProdutoLista.as_view(), name='lista_produtos'),
+    path('editar/<int:pk>/', views.ProdutoUpdate.as_view(), name='edita_produto'),  # noqa
+    path('deletar/<int:pk>/', views.ProdutoDeleta.as_view(), name='deleta_produto'),  # noqa
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .models import Produto
 from .forms import ProdutoForm
 
@@ -11,7 +11,19 @@ class ProdutoCreate(CreateView):
 
 class ProdutoLista(ListView):
     model = Produto
+    queryset = Produto.objects.all().order_by('-pk')
 
     def get_context_data(self, **produtos):
         context = super().get_context_data(**produtos)
         return context
+
+
+class ProdutoUpdate(UpdateView):
+    model = Produto
+    form_class = ProdutoForm
+    success_url = '/lista/'
+
+
+class ProdutoDeleta(DeleteView):
+    model = Produto
+    success_url = '/lista/'
