fbfd8a0a7752429631e2e97d4efd649e0370dc4c
Jefferson Duarte <jeffersonsduarte@live.com>
2022-08-24 10:21:33 +0100

    Adicionado função listar

--- produto/urls.py
+++ produto/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProdutoCreate.as_view(), name='index'),
+    path('', views.ProdutoCreate.as_view(), name='cadastro_produto'),
+    path('lista/', views.ProdutoLista.as_view(), name='lista_produtos')
 ]
--- produto/views.py
+++ produto/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Produto
 from .forms import ProdutoForm
 
@@ -7,3 +7,11 @@ class ProdutoCreate(CreateView):
     model = Produto
     form_class = ProdutoForm
     success_url = '/'
+
+
+class ProdutoLista(ListView):
+    model = Produto
+
+    def get_context_data(self, **produtos):
+        context = super().get_context_data(**produtos)
+        return context
