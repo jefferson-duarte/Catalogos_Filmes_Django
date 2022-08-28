a4366925101a9395f8664e18deeff9edc5f811ee
Jefferson Duarte <jeffersonsduarte@live.com>
2022-08-28 11:35:53 +0100

    Adicionado função login

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.CadastraUsuario.as_view(), name='cadastro_usuario'),
+    path('logar/', views.logar, name='logar_usuario'),
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.views.generic import CreateView
 from .forms import UsuarioForm
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.messages import constants
 
 
-class CadastraUsuario(CreateView):
+class CadastraUsuario(SuccessMessageMixin, CreateView):
     model = User
     form_class = UsuarioForm
     success_url = '/'
@@ -14,3 +18,17 @@ def logar(request):
     if request.method == 'POST':
         usuario = request.POST.get('usuario')
         senha = request.POST.get('senha')
+        user = authenticate(request, username=usuario, password=senha)
+
+        if not user:
+            messages.add_message(
+                request,
+                constants.ERROR,
+                'Usuário ou senha inválidos.'
+            )
+            return redirect('logar_usuario')
+        else:
+            login(request, user)
+            return redirect('lista_produtos')
+
+    return render(request, 'auth/login.html')
