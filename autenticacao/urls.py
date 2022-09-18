e4c009565a5e60874e0af74c5170b5fc6cf14a37
Jefferson Duarte <jeffersonsduarte@live.com>
2022-09-18 12:39:59 +0100

    Adicionado css no card produto-list

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cadastro/', views.CadastraUsuario.as_view(), name='cadastro_usuario'),
+    path('cadastro/', views.CadastraUsuario.as_view(), name='cadastro_usuario'),  # noqa
     path('logar/', views.logar, name='logar_usuario'),
     path('sair/', views.sair, name='logout'),
 ]
